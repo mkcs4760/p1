f119370f85a867fce9e1369087e65a721a628dcd
mkcs4760 <mk4cf@mail.umsl.edu>
2019-01-30 13:21:24 -0600

    Currently reading all of file with forked processes

--- p1.c
+++ p1.c
@@ -1,6 +1,6 @@
-//I've been working on p1 for several days now
-//by coding parts of the project in other test files
-//so now I began the process of putting it all together
+//As of now, we can read all the file input by the specific forked processes
+//Still need to add errors for when file does not match desired format
+//This is a huge step forward, however
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
 {
 	printf("Welcome to the project\n");
 	int i;
-	
 	
 	char inputFileName[] = "input.dat";
 	char outputFileName[] = "output.dat";
@@ -30,27 +29,48 @@ int main(int argc, char *argv[])
 	printf("The first number is our file is %d\n", childCount);
 	printf("Meaning we need to fork %d children to handle the file\n", childCount);
 	
+	int linesToPass = 1;
 	
 	pid_t pid;
 	for (i = 0; i < childCount; i++)
 	{
-			pid = fork();
-			if (pid > 0) {
-				printf("Don't look at me I'm only the parent\n");
-				wait(NULL);
-				continue; //I am the parent, create more children
-			}
-			else if (pid == 0) {
-				printf("This is a child with id %d\n", getpid());
-				exit(0);
-				break; //I am the child, get to work
-			}
-			else {
-				perror("Error while forking");
-				exit(EXIT_FAILURE);
-			}
-			
+		printf("Now let's shift over for the next kiddo \n");
+		char c;
+		int j;
+		for (j = 0; j < linesToPass; j++) { //we must pass over what the last child passed over so next child doesn't repeat work
+			do
+				c = fgetc(fp);
+			while (c != '\n');
+		}
+		linesToPass = 2;
 		
+		
+		pid = fork();
+		if (pid > 0) {
+			printf("Don't look at me I'm only the parent\n");
+			wait(NULL);
+			continue; //I am the parent, create more children
+		}
+		else if (pid == 0) {
+			printf("This is a child with id %d\n", getpid());
+			int sectionTotal;
+			fscanf(fp, "%1d", &sectionTotal);
+			printf("The first number in this section is %d\n", sectionTotal);
+			printf("The remaining %d numbers are: ", sectionTotal);
+			int j;
+			for (j = 0; j < sectionTotal; j++) {
+				int temp;
+				fscanf(fp, "%d", &temp);
+				printf(" %d ", temp);
+			}
+			printf("\n");
+			exit(0);
+			break; //I am the child, get to work
+		}
+		else {
+			perror("Error while forking");
+			exit(EXIT_FAILURE);
+		}
 	}
 	
 	printf("And now we're done\n");
