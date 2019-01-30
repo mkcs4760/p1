7e5613ffeb127189c6637d2629e93e18c6442dce
mkcs4760 <mk4cf@mail.umsl.edu>
2019-01-30 12:48:19 -0600

    Now wil fork as many copies as specified from input file

--- p1.c
+++ p1.c
@@ -4,38 +4,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 int main(int argc, char *argv[])
 {
 	printf("Welcome to the project\n");
+	int i;
+	
+	
+	char inputFileName[] = "input.dat";
+	char outputFileName[] = "output.dat";
 
+	FILE *fp;
+	fp = fopen(inputFileName, "r");
+	if (fp == NULL)
+	{
+		perror("Failed to locate file.");
+		exit(1);
+	}
+	printf("Opened file %s\n", inputFileName);
+
+	int childCount;
+	
+	fscanf(fp, "%1d", &childCount);
+
+	printf("The first number is our file is %d\n", childCount);
+	printf("Meaning we need to fork %d children to handle the file\n", childCount);
+	
+	
 	pid_t pid;
-
-	pid = fork();
-
-	if (pid < 0)
+	for (i = 0; i < childCount; i++)
 	{
-		perror("Error while forking");
-		exit(EXIT_FAILURE);
+			pid = fork();
+			if (pid > 0) {
+				printf("Don't look at me I'm only the parent\n");
+				wait(NULL);
+				continue; //I am the parent, create more children
+			}
+			else if (pid == 0) {
+				printf("This is a child with id %d\n", getpid());
+				exit(0);
+				break; //I am the child, get to work
+			}
+			else {
+				perror("Error while forking");
+				exit(EXIT_FAILURE);
+			}
+			
+		
 	}
-	else if (pid == 0)
-	{
-		int j;
-		for (j = 0; j < 10; j++)
-		{
-			printf("child: %d\n", j);
-			sleep(1);
-		}
-		exit(0);
-	}
-	else
-	{
-		int i;
-		for (i = 0; i < 10; i++)
-		{
-			printf("parent: %d\n", i);
-			sleep(1);
-		}
-		exit(0);
-	}
+	
+	printf("And now we're done\n");
+	
 	return 0;
 }
