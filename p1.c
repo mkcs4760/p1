3cb2437091d3eb37948fa8737bc66e317dcb90aa
mkcs4760 <mk4cf@mail.umsl.edu>
2019-02-02 14:55:43 -0600

    Did some cleaning up, polishing, and deleting code fragments that are no longer needed

--- p1.c
+++ p1.c
@@ -1,110 +1,58 @@
-//As of now, we can read all the file input by the specific forked processes
-//Still need to add errors for when file does not match desired format
-//This is a huge step forward, however
+//p1 is almost done
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
+#include <sys/types.h> //make sure you need all of these...
 #include <string.h>
 
-int MAXSIZE = 100;
-//int stack[];
 int top = -1;
-
-int isempty() {
-
-   if(top == -1)
-      return 1;
-   else
-      return 0;
-}
-   
-int isfull() {
-
-   if(top == MAXSIZE)
-      return 1;
-   else
-      return 0;
-}
-
-int peek(int stack[]) {
-	return stack[top];
-}
 
 int pop(int stack[]) {
    int data;
-	
-   if(!isempty()) {
-      data = stack[top];
-      top = top - 1;   
-      return data;
-   } else {
-      printf("Could not retrieve data, Stack is empty.\n");
-   }
+   data = stack[top];
+   top = top - 1;   
+   return data;
 }
 
-int push(int stack[], int data) {
-
-   if(!isfull()) {
-      top = top + 1;   
-      stack[top] = data;
-   } else {
-      printf("Could not insert data, Stack is full.\n");
-   }
+void push(int stack[], int data) {
+	top = top + 1;
+	stack[top] = data;
 }
-
 
 int main(int argc, char *argv[]) {
 	char inputFileName[] = "input.dat";
 	char outputFileName[] = "output.dat";
 	
-	printf("There are %d arguments\n", argc);
-	
 	int option;
-	char temp[100]; //setting max size as very high
 	while ((option = getopt(argc, argv, "hi:o:")) != -1) {
 		switch (option) {
-			case 'h' :
-						printf("You entered h\n"); //make sure everything needed is stated here
-						printf("Help page for OS_Klein_project1\n");
+			case 'h' :	printf("Help page for OS_Klein_project1\n");
 						printf("Consists of the following:\n\tOne .c file titled p1.c\n\tOne Makefile\n\tOne README.md file\n\tOne version control log.\n");
 						printf("The command 'make' will run the makefile and compile the program\n");
 						printf("Usage: ./p1 -i <inputFileName> -o <outputFileName> | ./p1 -h\n");
 						printf("Version control acomplished using github. Log obtained using command 'git log > versionLog.txt\n");
 						exit(0);
 						break;
-			case 'i' :
-						//&temp = optarg;
-						printf("You entered i\n");
-						printf("Argument is %s\n", optarg);
-						strcpy(inputFileName, optarg);
+			case 'i' :	strcpy(inputFileName, optarg);
 						break;
-			case 'o' :
-						printf("You entered o\n");
-						printf("Argument is %s\n", optarg);
-						strcpy(outputFileName, optarg);
+			case 'o' :	strcpy(outputFileName, optarg);
 						break;
 			default :	printf("You entered an invalid argument\n");
 		}
 	}
 	
 	printf("Welcome to the project\n");
-	int i;
-	//exit(0); //for testing
-	
 
 	FILE *input;
 	input = fopen(inputFileName, "r");
 	FILE *output;
 	output = fopen(outputFileName, "w");
-	if (input == NULL)
-	{
+	if (input == NULL) {
 		char errorString[100];
 		snprintf(errorString, sizeof errorString, "Failed to locate file %s ", inputFileName);
 		perror(errorString);
-		//error("Failed to locate file %s ", inputFileName);
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	printf("Opened file %s\n", inputFileName);
 
@@ -112,17 +60,13 @@ int main(int argc, char *argv[]) {
 	
 	fscanf(input, "%d", &childCount);
 
-	printf("The first number is our file is %d\n", childCount);
-	printf("Meaning we need to fork %d children to handle the file\n", childCount);
-	
 	int parentPid;
 	int listOfPids[childCount];
 	int linesToPass = 1; //the child processes don't change the parent's reading position, so we must pass ahead in the parent as well
 	
 	pid_t pid;
-	for (i = 0; i < childCount; i++)
-	{
-		printf("Now let's shift over for the next kiddo \n");
+	int i;
+	for (i = 0; i < childCount; i++) {
 		char c;
 		int j;
 		for (j = 0; j < linesToPass; j++) { //we must pass over what the last child passed over so next child doesn't repeat work
@@ -133,38 +77,27 @@ int main(int argc, char *argv[]) {
 		linesToPass = 2;
 		
 		pid = fork();
-		//listOfPids[i] = getpid();
-		if (pid > 0) {
-			printf("Don't look at me I'm only the parent\n");
+		if (pid > 0) { //parent case
 			parentPid = getpid();
 			wait(NULL);
-			printf("The child that just completed had the pid %d\n", pid);
 			listOfPids[i] = pid;
 			continue; //I am the parent, create more children
 		}
-		else if (pid == 0) {
-			printf("This is a child with id %d\n", getpid());
-			//listOfPids[i] = getpid();
-			printf("That's right, %d\n", listOfPids[i]);
+		else if (pid == 0) { //child case
 			int sectionTotal;
 			fscanf(input, "%d", &sectionTotal);
-			printf("The first number in this section is %d\n", sectionTotal);
-			printf("The remaining %d numbers are: ", sectionTotal);
 			int j;
 			int stack[sectionTotal];
 			for (j = 0; j < sectionTotal; j++) {
 				int temp;
 				fscanf(input, "%d", &temp);
-				printf(" %d ", temp);
 				push(stack, temp);
 			}
+			fprintf(output, "%d: ", getpid());
 			for (j = 0; j < sectionTotal; j++) {
 				fprintf(output, " %d ", pop(stack));
 			}
-			
-			printf("\n");
 			fprintf(output, "\n");
-			//fputs("\n", output);
 			exit(0);
 			break; //I am the child, get to work
 		}
@@ -173,17 +106,19 @@ int main(int argc, char *argv[]) {
 			exit(EXIT_FAILURE);
 		}
 	}
-	//listOfPids(childCount) = gitppid();
 	
 	fclose(input);
-	fclose(output);
-	printf("All children were: ");
-	for (i = 0; i < childCount; i++) {
-		printf(" %d ", listOfPids[i]);
-	}
-	printf("\n And the parent was %d\n", parentPid);
 	
-	printf("And now we're done\n");
+	fprintf(output, "All children were: ");
+	for (i = 0; i < childCount; i++) {
+		fprintf(output, " %d ", listOfPids[i]);
+	}
+	fprintf(output, "\nAnd the parent was %d\n", parentPid);
+	
+	fclose(output);
+	
+	printf("Results stored in %s\n", outputFileName);
+	printf("End of program\n");
 	
 	return 0;
 }
